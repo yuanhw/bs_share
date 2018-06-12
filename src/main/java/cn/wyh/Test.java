202a052946c674f5a5d15a072799453a3fa7f222
wangyuanhang <13588746259@163.com>
2018-06-12 23:39:54 +0800

    增加测试类

---
+++ src/main/java/cn/wyh/Test.java
@@ -0,0 +1,12 @@
+package cn.wyh;
+
+/**
+ * @author: WangYuanHang
+ * @Description:
+ * @date: 2018/6/12 23:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
